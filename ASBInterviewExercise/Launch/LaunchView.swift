b49b7d650fb7184a24cf81413e973fa1af8aa31e
Ray Liu <sevenray@gmail.com>
2025-03-04 21:53:44 +1300

    feat: Update project to launch swiftui view as main view

---
+++ ASBInterviewExercise/Launch/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  ASBInterviewExercise
+//
+//  Created by Ray Liu on 04/03/2025.
+//
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        VStack {
+            Text("hellow")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LaunchView()
+}
